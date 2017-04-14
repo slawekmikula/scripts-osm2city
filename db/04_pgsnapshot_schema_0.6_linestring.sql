f88c36d133ddee5312632a8cb9b9c74b85a1bbfb
Slawek Mikula <slawek.mikula@gmail.com>
2017-04-14 10:47:56 +0200

    - modified scripts for db creation - not symlinks but hard copy from osmosis

--- db/04_pgsnapshot_schema_0.6_linestring.sql
+++ db/04_pgsnapshot_schema_0.6_linestring.sql
@@ -1,1 +1,8 @@
-/mnt/media/loty/flightgear/osm2city/development/osmosis/script/pgsnapshot_schema_0.6_linestring.sql
+-- Add a postgis GEOMETRY column to the way table for the purpose of storing the full linestring of the way.
+SELECT AddGeometryColumn('ways', 'linestring', 4326, 'GEOMETRY', 2);
+
+-- Add an index to the bbox column.
+CREATE INDEX idx_ways_linestring ON ways USING gist (linestring);
+
+-- Cluster table by geographical location.
+CLUSTER ways USING idx_ways_linestring;
